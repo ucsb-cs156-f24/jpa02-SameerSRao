a23494be60d96373f04848ecfc9178d33bf8152a
Sameer Rao <sameerrao@169-231-50-112.wireless.ucsb.edu>
2024-10-14 21:25:21 -0700

    updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,11 +25,16 @@ public class DeveloperTest {
         // TODO: Replace Chris G. with your name as shown on
         // <https://bit.ly/cs156-f24-teams>
         assertEquals("Sameer", Developer.getName());
-        // assertEquals("SameerSRao", Developer.getGithubId());
-        // assertEquals("Sameer", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        // TODO: Replace Chris G. with your name as shown on
+        // <https://bit.ly/cs156-f24-teams>
+        assertEquals("SameerSRao", Developer.getGithubId());
+    }
 
 }
